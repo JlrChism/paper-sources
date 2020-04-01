b314edb4edb798c4a2d96f4b08b2da19c4576c99
JlrChism <63009278+JlrChism@users.noreply.github.com>
2020-04-01 14:34:09 -0500

    Add files via upload

--- EglishPaperCode/src/eglishpapercode/EnglishPaperCode.java
+++ EglishPaperCode/src/eglishpapercode/EnglishPaperCode.java
@@ -21,7 +21,9 @@ public class EnglishPaperCode {
         g1.setGrade2(95);
         g1.setGrade3(56);
         
-        g1.setAverage(g1.getGrade1()+g1.getGrade2()+g1.getGrade3()/3);
+        g1.setAverage((g1.getGrade1()+g1.getGrade2()+g1.getGrade3())/3);
+        
+         
         
      System.out.println("The grade of " + g1.getStudent1() + " is " + g1.getGrade1());   
      System.out.println("The grade of " + g1.getStudent2() + " is " + g1.getGrade2());  
